949ac7985d666492a30d7d57c0da9c417c96957a
Vasyl-Polyanskyy <119352065+Vasyl-Polyanskyy@users.noreply.github.com>
2023-02-19 19:34:37 +0100

    :rocket: feat: add map for links

--- src/components/Layout/LayoutMain.tsx
+++ src/components/Layout/LayoutMain.tsx
@@ -1,34 +1,52 @@
 import { FC, ReactNode } from "react";
 import Link from "next/link";
 
+const SECTIONS = [
+  {
+    title: "Return",
+    link: "/return",
+  },
+  {
+    title: "Services",
+    link: "/services",
+  },
+  {
+    title: "Our work",
+    link: "/our-work",
+  },
+  {
+    title: "About us",
+    link: "/about-us",
+  },
+  {
+    title: "Our workers",
+    link: "/our-workers",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
 interface LayoutMainProps {
   children: ReactNode;
 }
 
 const LayoutMain: FC<LayoutMainProps> = ({ children }) => {
   return (
-    <div className="bg-neutral-900 w-full h-screen flex flex-col overflow-hidden relative">
-      {children}
-      <div className="justify-between flex flex-col h-screen items-start py-10">
-        <Link href="/" className="text-white text-xl rotate-90">
-          Return
-        </Link>
-        <Link href="/" className="text-primary text-xl rotate-90">
-          Services
-        </Link>
-        <Link href="/" className="text-white text-xl rotate-90">
-          Our work
-        </Link>
-        <Link href="/" className="text-white text-xl rotate-90">
-          About us
-        </Link>
-        <Link href="/" className="text-white text-xl rotate-90">
-          Our workers
-        </Link>
-        <Link href="/" className="text-white text-xl rotate-90">
-          Contact
-        </Link>
+    <div className="bg-neutral-900 w-full h-screen flex flex-row overflow-hidden relative pl-6">
+      <div className="flex flex-col items-center h-full gap-32 justify-center">
+        {SECTIONS.map((section) => (
+          <Link
+            key={section.title}
+            href={section.link}
+            className="text-white text-xl [writing-mode:vertical-lr] [text-orientation:mixed] rotate-180"
+          >
+            {section.title}
+          </Link>
+        ))}
       </div>
+      {children}
     </div>
   );
 };
