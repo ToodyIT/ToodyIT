6086b80f061f787ac9a4820d64c30faa5c7394b1
Ivan Hodynchuk <ivan.hodynchuk@shopsys.com>
2023-06-12 21:51:00 +0300

    Add supports for old browsers with height 100v

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -68,7 +68,8 @@ const HomePage: NextPage = () => {
           "Discover ToodyIT, a leading web solutions provider specializing in tailored websites to meet your unique needs. Our expert team delivers innovative designs, seamless development, and strategic solutions for your online success."
         )}
       />
-      <div className="bg-neutral-900 w-full h-[100dvh] overflow-hidden flex flex-col relative">
+      <div className="bg-neutral-900 w-full overflow-hidden flex flex-col relative" id="homepage-wrapper">
+        
         <HomepageHeaderMenu key="header" isOpen={openedSection === "header"} />
         <div className="mx-auto flex flex-col h-full w-full">
           <HomepageHeader />
