ef03171aa13f973a1c7b0d025e5b414ab78da03e
Ivan Hodynchuk <ivan.hodynchuk@shopsys.com>
2023-06-12 14:49:36 +0300

    Add blurred photo

--- src/pages/our-team.page.tsx
+++ src/pages/our-team.page.tsx
@@ -5,41 +5,47 @@ import SlideAnimationLayout from "../components/Layout/SlideAnimationLayout";
 import { useTranslation, TFunction } from "next-i18next";
 import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import IvanHodynchukPhoto from "/public/img/our-team/ivan-hodynchuk.jpg";
+import VasylPolyasnkyyPhoto from "/public/img/our-team/vasyl-polyanskyy.jpg";
+import NatalieLysenkoPhoto from "/public/img/our-team/natalie-lysenko.jpg";
+import AndriyKosarkoPhoto from "/public/img/our-team/andriy-kosarko.jpg";
+import JakubDvoracekPhoto from "/public/img/our-team/jakub-dvoracek.jpg";
 
 const getOurTeam = (t: TFunction) => {
   return [
     {
-      imagePath: "vasyl-polyanskyy.jpg",
+      imagePath: VasylPolyasnkyyPhoto,
       name: t("VASYL POLYANSKYY"),
       position: t("PROJECT MANAGER"),
       alt: t("Vasyl Polyanskyy"),
     },
     {
-      imagePath: "ivan-hodynchuk.jpg",
+      imagePath: IvanHodynchukPhoto,
       name: t("IVAN HODYNCHUK"),
       position: t("DEVELOPER"),
       alt: t("Ivan Hodynchuk"),
     },
     {
-      imagePath: "natalie-lysenko.jpg",
+      imagePath: NatalieLysenkoPhoto,
       name: t("NATALIE LISENKO"),
       position: t("DESIGNER"),
       alt: t("Natalie Lysenko"),
     },
     {
-      imagePath: "andriy-kosarko.jpg",
+      imagePath: AndriyKosarkoPhoto,
       name: t("ANDRIY KOSAREV"),
       position: t("TRANSLATOR"),
       alt: t("Andriy Kosarko"),
     },
     {
-      imagePath: "jakub-dvoracek.jpg",
+      imagePath: JakubDvoracekPhoto,
       name: t("JAKUB DVOŘÁČEK"),
       position: t("DEVELOPER"),
       alt: t("Jakub Dvořáček"),
     },
   ];
 };
+
 const OurTeamPage = forwardRef<HTMLDivElement>((_, ref) => {
   const [emblaRef] = useEmblaCarousel({ align: "start" });
   const { t } = useTranslation();
@@ -53,7 +59,7 @@ const OurTeamPage = forwardRef<HTMLDivElement>((_, ref) => {
         "Meet our talented and dedicated team of professionals. Discover the expertise and passion that drives us to create exceptional websites. Learn about our diverse skills and experience, as well as our collaborative approach to delivering high-quality web solutions. Get to know the individuals behind our success."
       )}
     >
-      <div className="flex flex-col gap-7 -mr-5">
+      <div className="flex flex-col gap-7 -mr-5 h-full">
         <h1 className="text-white font-bold text-3xl flex text-center">
           {t("OUR TEAM")}
         </h1>
@@ -70,14 +76,12 @@ const OurTeamPage = forwardRef<HTMLDivElement>((_, ref) => {
                 <div className="p-5 flex gap-5 flex-col">
                   <div className="relative h-[350px] w-[277px] lg:h-[400px] lg:w-[327px] ">
                     <Image
-                      src={`/img/our-team/${team.imagePath}`}
+                      src={team.imagePath}
                       alt={team.alt}
-                      width={327}
-                      height={400}
+                      placeholder="blur"
                       className="rounded-3xl"
                     />
                   </div>
-
                   <div className="flex flex-col gap-1">
                     <h2 className="text-primary">{team.name}</h2>
                     <span>{team.position}</span>
