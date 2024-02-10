a55452240d59cbdd6f7bbc91d4a453ac077c88f1
Ivan Hodynchuk <ivan.hodynchuk@shopsys.com>
2024-02-10 15:53:43 +0100

    Add cookie popup

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,10 +1,17 @@
 import { Html, Head, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   return (
     <Html>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Script
+          strategy="lazyOnload"
+          id="cookieyes"
+          type="text/javascript"
+          src="https://cdn-cookieyes.com/client_data/44d0af6cd902359de29ba189/script.js"
+        />
       </Head>
       <body>
         <NextScript />
