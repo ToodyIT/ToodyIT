1bce3d5810de15115bb3d393a7af042587ecebb3
Ivan Hodynchuk <ivan.hodynchuk@shopsys.com>
2024-02-10 16:08:44 +0100

    Add google tag amanger

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -12,6 +12,19 @@ const Document = () => {
           type="text/javascript"
           src="https://cdn-cookieyes.com/client_data/44d0af6cd902359de29ba189/script.js"
         />
+        <Script
+          id="gtm"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TP6V5HW6');
+            `,
+          }}
+        />
       </Head>
       <body>
         <NextScript />
