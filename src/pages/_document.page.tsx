dc56b58d4ec0e6a718bb4934f4198e3d7daf00e9
Ivan Hodynchuk <ivan.hodynchuk@shopsys.com>
2024-03-17 17:44:20 +0100

    Rollback gtm

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -21,10 +21,11 @@ const Document = () => {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TP6V5HW6');
+            })(window,document,'script','dataLayer','GTM-MHFCFRL3');
             `,
           }}
         />
+
         {/* <Script
           id="gtagAds"
           async
